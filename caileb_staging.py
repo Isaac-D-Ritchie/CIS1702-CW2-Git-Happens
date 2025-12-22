5031c93adb508e95b2fa9da07b82df2c1e474140
Cherino <cookcaileb99@gmail.com>
2025-12-22 15:24:58 +0000

    Added more comments, created a function for nextday() under TimeBreak Class and added some error handling along with #log comments to sort soon.

--- caileb_staging.py
+++ caileb_staging.py
@@ -1,4 +1,5 @@
 import datetime as dt
+from datetime import timedelta
 import json
 import urllib.request
 import statistics 
@@ -10,9 +11,9 @@ class WeatherAPIHandler:
         self.units = units
     
         # API URLS AND KEYS
-        self.api_data = [ # Need to change after 2.5/ to include /forecast and /weather depending on report type
+        self.api_data = [
             ("https://api.openweathermap.org/data/2.5/forecast", "54bb47ed4ca69c0d0ca37bcb79711b5f"), # OWM Primary
-            ("https://api.openweathermap.org/data/2.5/weather", "011d9bcc33d58317a8ca04ae04c52d91"), # OWM Backup
+            ("https://api.openweathermap.org/data/2.5/forecast", "011d9bcc33d58317a8ca04ae04c52d91"), # OWM Backup
             ("https://backupapi.example.com/data", "fillerkey1234"),# Backup API Primary
             ("https://backupapi.example.com/data", "anotherfillerkey5678")# Backup API Backup
             ]
@@ -20,7 +21,7 @@ class WeatherAPIHandler:
     def connect(self, index = 0):
         #Index is the iterator in list self.api_data and when end of list is spotted no connection found.
         if index >= len(self.api_data):
-            print("CRITICAL ERROR: ALL API CONNECTION DATA FAILED, EXITING") #Create log
+            print("CRITICAL ERROR: ALL API CONNECTION DATA FAILED, EXITING") #LOG
             return None
         
         baseurl, key = self.api_data[index] #Var A & B share out paired items in I's point in the list of pairs
@@ -35,11 +36,11 @@ class WeatherAPIHandler:
         try:
             with urllib.request.urlopen(full_url, timeout=10) as response:
                 if response.status == 200:
-                    print("Connection successful!") 
-                    return json.load(response) #AND LOG
+                    print("Connection successful!") #LOG 
+                    return json.load(response) #LOG
         except Exception as e: #Recursive step given failed connection
-            print(f"Error: {e}\nAttempt {index+1} failed, Trying next option...")
-            return self.connect(index+1) #AND LOG with Error!
+            print(f"Error: {e}\nAttempt {index+1} failed, Trying next option...")#LOG
+            return self.connect(index+1) #LOG
 #! TB = Testing TimeBreak
 class TimeBreak:
     def __init__(self):
@@ -51,67 +52,107 @@ class TimeBreak:
         self.day = str(now.day)
         self.hour = str(now.hour)
         self.minute = str(now.minute)
+        
+        '''def nextday(index):
+            if index > 0:
+                self.day = self.day + timedelta(days=index)
+                return self.day, nextday(index-1)
+            else:
+                None
+        !!NEEDS WORK
+        '''    
 
         self.reporting_date = f"{self.day}/{self.month}" # in terminal reporting
         self.filestamp = f"|{self.day}/{self.month}/{self.year} - {self.hour}:{self.minute}|" # For new entry appended to Reporting file
         
-
+def backout():
+    print("Backing Out...\n",("="*25),("\n"*5))
+    #LOG
 # MENU FUNCTIONS
 
 def one_daydata():
     """One Day Data for One City and Offers Report Type"""
-    city = input("Enter City name: ")
-    handler = WeatherAPIHandler(city, "metric")
-    time = TimeBreak() #Needs testing and implemented first ! TB
-    data = handler.connect()
+    try:
+        city = input("Enter City name: ")
+        handler = WeatherAPIHandler(city, "metric")
+        time = TimeBreak()
+        data = handler.connect()
+    except Exception as e:
+        print(f"\nError occurred:{e}") #LOG
+        backout()
 
     if not data:
-        print("Error: Could not retrieve data.") # AND LOG
+        print("Error: Could not retrieve data.") #LOG
         return
 
     # Take first 8 items of forecast
     try:
-        next_24h = data['list'][:8]
+        next_24h = data['list'][:8] #LOG
     except:
-        print(data)
+        print(data) #LOG
+
     print("\nSelect Report Level:")
     print("1. Simple (Summary)")
     print("2. In-Depth (Hourly Table)")
+    print("3. Back to Menu")
     choice = input("Selection: ")
+    
     if choice == "1":
         # Simple Report
         # next_24h has 8 items, temps and conditions stores 8 item list of values
-        temps = [item['main']['temp'] for item in next_24h] 
-        conditions = [item['weather'][0]['main'] for item in next_24h]
+        try:
+            temps = [item['main']['temp'] for item in next_24h] 
+            conditions = [item['weather'][0]['main'] for item in next_24h]
+        except Exception as e:
+            print(f"Error Occured:{e}")#LOG
+            backout()
         # 'pop' is probability of precipitation (0.0 to 1.0) 
-        rain_chance = next_24h[0].get('pop', 0) * 100 
+        rain_chance = next_24h[0].get('pop', 0) * 100 #LOG
         
-        avg_temp = sum(temps) / len(temps) # simple mean
-        most_common_weather = statistics.mode(conditions) #most common weather type in day
-#! TB
-        print(f"\n--- Summary for {city.title()} on the date: {time.reporting_date} ---")
-        print(f"Average Temp: {avg_temp:.1f}°C")
-        print(f"High/Low:     {max(temps)}°C / {min(temps)}°C")
-        print(f"Main Weather: {most_common_weather}")
+        avg_temp = sum(temps) / len(temps) # simple mean #LOG
+        most_common_weather = statistics.mode(conditions) #most common weather type in day #LOG
+
+        #Simple Reporting Printing
+        try:
+            print(f"\n--- Summary for {city.title()} -- Date: {time.reporting_date} ---")
+            print(f"Average Temp: {avg_temp:.1f}°C")
+            print(f"High/Low:     {max(temps)}°C / {min(temps)}°C")
+            print(f"Main Weather: {most_common_weather}")
         
-        # Rain Bar Chart  '*' = 5%
-        stars = "*" * int(rain_chance / 5)
-        print(f"Rain Chance:  [{stars:<20}] {rain_chance:.1f}%")
+            # Rain Bar Chart  '*' = 5%
+            stars = "*" * int(rain_chance / 5)
+            print(f"Rain Chance:  [{stars:<20}] {rain_chance:.1f}%")
+        except Exception as e:
+            print(f"Error Occurred:{e}")#LOG
+            backout() 
 
     elif choice == "2":
-#! TB      In-Depth reporting 
-        print(f"Detailed Forecast for {city.title()} on the date: {time.reporting_date}
-        print(f"\n{'Time':<12} | {'Temp':<8} | {'Weather':<12} | {'Wind':<8} | {'Humidity'}")
-        print("-" * 60)
-        for hour in next_24h:
-            # Slices "2025-12-21 15:00:00" into "15:00" on time_label
-            time_label = hour['dt_txt'][11:16]
-            temp = hour['main']['temp']
-            weather = hour['weather'][0]['main']
-            wind = hour['wind']['speed']
-            humid = hour['main']['humidity']
-            
-            print(f"{time_label:<12} | {temp:<8}°C | {weather:<12} | {wind:<8} | {humid}%")
+        #In-depth Reporting Printing
+        try:
+            print(f"\n---Detailed Forecast for {city.title()} -- Date: {time.reporting_date} ---")
+            print(f"\n{'Time':<12} | {'Temp':<8} | {'Weather':<12} | {'Wind':<8} | {'Humidity'}")
+            print("-" * 60)
+            #Prints Iterates through 3 hour intervals
+            for hour in next_24h:
+                # IMPLEMENT TO FIX CONFUSING REPORTING
+                #!!if int(hour[:1]) < int(hour-1[:1]) == True:
+                #!!    print("Tomorrow:", TimeBreak.nextday(1))
+
+                # Slices "2025-12-21 15:00:00" into "15:00" on time_label
+                time_label = hour['dt_txt'][11:16]
+                temp = hour['main']['temp']
+                weather = hour['weather'][0]['main']
+                wind = hour['wind']['speed']
+                humid = hour['main']['humidity']
+                
+                print(f"{time_label:<12} | {temp:<8}°C | {weather:<12} | {wind:<8} | {humid}%")
+
+        except Exception as e:
+            print(f"Error Occurred:{e}")#LOG
+            backout()
+
+    elif choice == "3":
+        return backout()
 
 #MAIN MENU
 def weather_menu():
@@ -120,16 +161,20 @@ def weather_menu():
         print("\n" + "="*25 + "\n Weather System Menu\n" + "="*25)
         print("1. Get One Day Report")
         print("2. Quit")
-        
-        userinput = input("\nPlease enter choice: ")
-        
-        if userinput == "1":
-            one_daydata()
-        elif userinput == "2":
-            print ("Ending Session...")
-            break
-        else:
-            print("Invalid input. Try again.")
+        try:
+            userinput = input("\nPlease enter choice: ")
+            
+            if userinput == "1":
+                one_daydata()
+            #elif userinput ==  "2":
+                #week_data() BUILD MODULE
+            elif userinput == "2":
+                print ("Ending Session...")
+                break
+            else:
+                print("Invalid input. Try again.")#LOG
+        except Exception as e:
+            print(e,"Silly guy!")#LOG
 #MAIN
 def main():
     weather_menu()
