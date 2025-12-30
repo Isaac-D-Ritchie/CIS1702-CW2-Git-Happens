9988906e18f2c93bc3c7d86f3a1e3931cfb3a7ef
Benjamin-el2005 <benjamin.el2005@gmail.com>
2025-12-30 23:13:12 +0000

    Added a feature to allow the user to select specific hours

--- Weather_API_UI.py
+++ Weather_API_UI.py
@@ -1,10 +1,17 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 
 # creates root window
 root = tk.Tk(screenName="Weather Report Generator", baseName="Weather Report Generator", className="Weather Report Generator")
 root.geometry("450x300")
+
+#creates variables for dropdown menus
+
+hours = ("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", 
+         "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", 
+         "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", )
 
 #creates grid weights for easier placement of widgets
 
@@ -17,24 +24,48 @@ root.rowconfigure(2, weight=1)
 def basic_report():
     location = location_entry.get()
     date = date_entry.get()
-    messagebox.showinfo(title="Basic Weather Report", message=f"The weather in {location} on {date} will be xyz")
+    hour = hour_dropdown.get()
+    if (check_state.get() == 0):
+        messagebox.showinfo(title="Basic Weather Report", message=f"The weather in {location} on {date} will be xyz")
+    else:
+        messagebox.showinfo(title="Basic Weather Report", message=f"The weather in {location} on {date} at {hour} will be xyz")
 
 #function for generating a detailed report
 
 def detailed_report():
     location = location_entry.get()
     date = date_entry.get()
-    messagebox.showinfo(title="Detailed Weather Report", message=f"A more detailed report of\n the weather in {location} on {date} would be xyz")
+    hour = hour_dropdown.get()
+    if (check_state.get() == 0):
+        messagebox.showinfo(title="Detailed Weather Report", message=f"A more detailed report of\n the weather in {location} on {date} would be xyz")
+    else:
+        messagebox.showinfo(title="Detailed Weather Report", message=f"A more detailed report of\n the weather in {location} on {date} at {hour} \nwould be xyz")
+
+#function to determine whether the hour dropdown menu should be enabled or disabled
+
+def hour_dropdown_check():
+    if (check_state.get() == 0):
+        hour_dropdown.configure(state="disabled")
+    else:
+        hour_dropdown.configure(state="enabled")
 
 # creates labels so user knows where to inpurt data
 
 location_label = Label(root, text="Please enter a location:")
 date_label = Label(root, text="Please enter a date (YYYY-MM-DD):")
+hour_label = Label(root, text="Add an hour?")
 
-#creates entries to take data inputs from the user
+#creates widgets to take data inputs from the user
 
 location_entry = Entry(root)
 date_entry = Entry(root)
+
+#creates checkbox and listbox for hour values
+
+check_state = tk.IntVar()
+hour_checkbox = Checkbutton(root, variable=check_state, onvalue=1, offvalue=0, command=hour_dropdown_check)
+hour_dropdown = ttk.Combobox(root, values=hours, state=DISABLED)
+hour_dropdown.set("Please select an hour")
 
 #creates button to generate report
 
@@ -45,9 +76,12 @@ detailed_report_btn = Button(root, text="Generate In-Depth Report", command=deta
 
 location_label.grid(row=0, column=0, pady=2, padx=2, sticky=tk.W+tk.E)
 date_label.grid(row=1, column=0, pady=2, padx=2, sticky=tk.W+tk.E)
+hour_label.grid(row=2, column=0, pady=2, padx=2, sticky=tk.W+tk.E+tk.N)
 
 location_entry.grid(row=0, column=1, pady=2, padx=2, sticky=tk.W+tk.E)
 date_entry.grid(row=1, column=1, pady=2, padx=2, sticky=tk.W+tk.E)
+hour_checkbox.grid(row=2, column=0, pady=2, padx=2, sticky=tk.E+tk.N)
+hour_dropdown.grid(row=2, column=1, pady=2, padx=2, sticky=tk.N)
 
 report_btn.grid(row=2, column=0, sticky=tk.S+tk.W+tk.E)
 detailed_report_btn.grid(row=2, column=1, sticky=tk.S+tk.W+tk.E)
